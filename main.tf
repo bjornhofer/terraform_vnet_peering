b134986c093b6da04c9b03a695a884365b3a699c
bjornhofer <bjoern.hoefer@microsoft.com>
2023-01-25 15:44:18 +0000

    resource_group fixed

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network_peering" "source2dest" {
   count                        = var.dbg_simulate ? 0:1
   name                         = var.vnet_peering_name == "peering" ? "peering-${split("/", var.vnet_peering_source_vnet)[8]}_${split("/", var.vnet_peering_destination_vnet)[8]}" : var.vnet_peering_name
-  resource_group_name          = var.vnet_peering_resource_group
+  resource_group_name          = split("/", var.vnet_peering_source_vnet)[4]
   virtual_network_name         = split("/", var.vnet_peering_source_vnet)[8]
   remote_virtual_network_id    = var.vnet_peering_destination_vnet
   allow_virtual_network_access = var.allow_virtual_network_access
@@ -12,7 +12,7 @@ resource "azurerm_virtual_network_peering" "source2dest" {
 resource "azurerm_virtual_network_peering" "dest2main" {
   count                        = var.dbg_simulate ? 0:1
   name                         = var.vnet_peering_name == "peering" ? "peering-${split("/", var.vnet_peering_destination_vnet)[8]}_${split("/", var.vnet_peering_source_vnet)[8]}" : var.vnet_peering_name
-  resource_group_name          = var.vnet_peering_resource_group
+  resource_group_name          = split("/", var.vnet_peering_destination_vnet)[4]
   virtual_network_name         = split("/", var.vnet_peering_destination_vnet)[8]
   remote_virtual_network_id    = var.vnet_peering_source_vnet
   allow_virtual_network_access = var.allow_virtual_network_access
