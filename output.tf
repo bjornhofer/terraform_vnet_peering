ca9a26090276e5096ca7c55c1321d5f268c551ad
bjornhofer <bjoern.hoefer@microsoft.com>
2023-01-25 15:37:20 +0000

    fixed output

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
-output "peering" {
-  value = azurerm_virtual_network_peering.main
+output "source2dest" {
+  value = azurerm_virtual_network_peering.source2dest
+}
+
+output "dest2main" {
+  value = azurerm_virtual_network_peering.dest2main
 }
