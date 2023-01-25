dbb969eb745a912ad08998fa3542d56fa2a1b626
bjornhofer <bjoern.hoefer@microsoft.com>
2023-01-25 15:29:22 +0000

    fixed wrong index

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network_peering" "main" {
   count                        = var.dbg_simulate ? 0:1
-  name                         = var.vnet_peering_name == "peering" ? "peering-${split("/", var.vnet_peering_source_vnet)[7]}_${split("/", var.vnet_peering_destination_vnet)[7]}" : var.vnet_peering_name
+  name                         = var.vnet_peering_name == "peering" ? "peering-${split("/", var.vnet_peering_source_vnet)[8]}_${split("/", var.vnet_peering_destination_vnet)[8]}" : var.vnet_peering_name
   resource_group_name          = var.vnet_peering_resource_group
   virtual_network_name         = var.vnet_peering_source_vnet
   remote_virtual_network_id    = var.vnet_peering_destination_vnet
